60329eda19a2ee39300623c034a7a5920fc1a435
Usama <usama.zubair@snappymob.com>
2026-05-10 17:33:38 +0800

    sign in , sign up and forgot passoword page design

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+   before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name,
+      :last_name,
+      :identification_type,
+      :identification_number
+    ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name,
+      :last_name,
+      :identification_type,
+      :identification_number
+    ])
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   get "user_profiles/index"
   get "user_profiles/show"
   get "user_profiles/new"
